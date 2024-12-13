67b863615c334f280591de7e11fe9c92430b32e6
seongho220905 <seonhgo.lim@diquest.com>
2024-12-13 14:12:04 +0900

    feat: Apply TypeScrip

--- src/utils/pathHelper.js
+++
@@ -1,6 +0,0 @@
-const p = require('path');
-
-exports.printPaths = () =>{
-    console.log('process.cwd() : ', process.cwd());
-
-}
---
+++ src/utils/pathHelper.ts
@@ -0,0 +1,5 @@
+const p = require('path');
+
+exports.printPaths = () => {
+  console.log('process.cwd() : ', process.cwd());
+};
